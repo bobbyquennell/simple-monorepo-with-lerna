b5e549025ed433087800d03053b2eedace43fcca
Bobby Wang <bobbyquennell@gmail.com>
2021-06-26 00:22:10 +1000

    update

--- packages/app/.lintstagedrc.js
+++ packages/app/.lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   '**/*.{js?(x),ts?(x)}': () => 'npm run lint:fix',
-  '**/*.ts?(x)': () => 'npm run ts-check',
+  '**/*.ts?(x)': () => 'npm run ts-check', // add ts error check in lint-staged: https://stackoverflow.com/questions/60514929/eslint-not-reporting-typescript-compiler-type-checking-errors
   //'**/*.{js?(x),ts?(x)}': () => 'npm run lint:fix && npm run ts-check',
   // "*.{scss,css,md,html}": "prettier --write",
 }
