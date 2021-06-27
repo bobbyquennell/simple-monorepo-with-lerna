10c45774986402af80e48561bc4bea65eebb0d57
Bobby Wang <bobbyquennell@gmail.com>
2021-06-28 00:21:16 +1000

    update jest.config.js with some comments

--- jest.config.js
+++ jest.config.js
@@ -15,3 +15,12 @@ module.exports = {
   collectCoverageFrom: ["packages/*/src/**/*.{js,jsx,ts,tsx}"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
+
+//TODO:
+/*
+To use ts-jest in a project with monorepo structure, you'll need to use Jest projects configuration.
+When using Jest projects configuration, Jest will run ts-jest against each project which is defined in the configuration.
+*/
+// https://kulshekhar.github.io/ts-jest/docs/guides/using-with-monorepo
+
+//https://github.com/kulshekhar/ts-jest/issues/364#issuecomment-647506833
